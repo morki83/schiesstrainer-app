076660c40ae87e3d8cff57e6021abd5562ee9592
morki83 <121301891+morki83@users.noreply.github.com>
2025-05-21 13:35:37 +0200

    Delete svelte.config.js

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter()
-  }
-};
